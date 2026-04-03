4c5eddc7ace56970dff54cedfcde39ef59db908d
Rishihack <109895080+Rishihack@users.noreply.github.com>
2026-04-03 19:54:36 +0530

    Add files via upload

---
+++ data/HOSPITAL_4.js
@@ -0,0 +1,1 @@
+var json_HOSPITAL_4 = {"type":"FeatureCollection","name":"HOSPITAL_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null},"geometry":{"type":"MultiPoint","coordinates":[[84.040312506718593,21.861049292495654]]}}]}
